d7c63f07565f3b2eceebc77b8c90d7354dc597b9
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-10-16 11:58:25 -0400

    Adjustments

--- src/app/public/modules/shared/lookup-resources-provider.ts
+++ src/app/public/modules/shared/lookup-resources-provider.ts
@@ -1,13 +1,8 @@
 import {
-  Observable
-} from 'rxjs/Observable';
-
-import {
+  SkyAppLocaleInfo,
   SkyLibResourcesProvider
 } from '@skyux/i18n';
 
 export class SkyLookupResourcesProvider implements SkyLibResourcesProvider {
-  public getDefaultString: (name: string, ...args: any[]) => string;
-
-  public getString: (name: string, ...args: any[]) => Observable<string>;
+  public getString: (localeInfo: SkyAppLocaleInfo, ...args: any[]) => string;
 }
