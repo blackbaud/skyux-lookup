e73fd84f29df49239f4f93e40c4f663fb77b1e78
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-11-14 10:49:53 -0500

    Adding another fixture detectChanges

--- src/app/public/modules/autocomplete/autocomplete.component.spec.ts
+++ src/app/public/modules/autocomplete/autocomplete.component.spec.ts
@@ -416,6 +416,7 @@ describe('Autocomplete component', () => {
 
       // The search method should not execute at this time.
       tick(10);
+      fixture.detectChanges();
       expect(spy).not.toHaveBeenCalled();
 
       // Wait for the remaining debounce time.
