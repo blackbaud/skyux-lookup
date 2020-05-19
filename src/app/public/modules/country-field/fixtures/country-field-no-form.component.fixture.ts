256149466ce0fa01b1a149df5396f14f72502809
Trevor Burch <Trevor.Burch@blackbaud.com>
2020-05-19 16:22:37 -0400

    Allow for limiting the countries returned for the country field (#89)
    * Initial work and testing
    
    * Finished implementation
    
    * PR feedback
    
    * fixed tests
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    Co-authored-by: Alex <alex.kingman@blackbaud.com>

--- src/app/public/modules/country-field/fixtures/country-field-no-form.component.fixture.ts
+++ src/app/public/modules/country-field/fixtures/country-field-no-form.component.fixture.ts
@@ -26,6 +26,8 @@ export class CountryFieldNoFormTestComponent {
 
   public isRequired: boolean = false;
 
+  public supportedCountryISOs: string[];
+
   public countryChanged(country: SkyCountryFieldCountry): void {
     return;
   }
