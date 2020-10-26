ba3154beaa470a36dfc9456ae68e623bb0d7c072
Paul Crowder <paul.crowder@blackbaud.com>
2020-10-26 15:15:35 -0400

    Lookup modern theme (#142)
    * Lookup modern theme
    
    * Added test

--- e2e/lookup.e2e-spec.ts
+++ e2e/lookup.e2e-spec.ts
@@ -7,24 +7,47 @@ import {
 
 import {
   expect,
-  SkyHostBrowser
+  SkyHostBrowser,
+  SkyVisualThemeSelector
 } from '@skyux-sdk/e2e';
 
 describe('Lookup component', () => {
-  function validateScreenshot(done: DoneFn, screenshotName: string) {
+  let currentTheme: string;
+  let currentThemeMode: string;
+
+  async function selectTheme(theme: string, mode: string): Promise<void> {
+    currentTheme = theme;
+    currentThemeMode = mode;
+
+    return SkyVisualThemeSelector.selectTheme(theme, mode);
+  }
+
+  function getScreenshotName(name: string): string {
+    if (currentTheme) {
+      name += '-' + currentTheme;
+    }
+
+    if (currentThemeMode) {
+      name += '-' + currentThemeMode;
+    }
+
+    return name;
+  }
+
+  function validateScreenshot(done: DoneFn, screenshotName: string): void {
     expect('#lookup-visual').toMatchBaselineScreenshot(done, {
-      screenshotName
+      screenshotName: getScreenshotName(screenshotName)
     });
   }
 
-  function validateScreenshotWithMenu(done: DoneFn, screenshotName: string) {
+  async function validateScreenshotWithMenu(done: DoneFn, screenshotName: string): Promise<void> {
     const input = element(by.css('textarea'));
     input.value = 'r';
-    input.click();
+    await input.click();
 
-    browser.actions().sendKeys('r').perform();
+    await browser.actions().sendKeys('r').perform();
 
-    browser.wait(
+    await browser.wait(
       ExpectedConditions.presenceOf(element(by.css('.sky-autocomplete-results'))),
       1200,
       'Autocomplete results dropdown took too long to appear.'
@@ -33,42 +56,63 @@ describe('Lookup component', () => {
     validateScreenshot(done, screenshotName);
   }
 
-  beforeEach(() => {
-    SkyHostBrowser.get('visual/lookup');
+  function runTests(): void {
+    describe('(lg screens)', () => {
+      beforeEach(async () => {
+        await SkyHostBrowser.setWindowBreakpoint('lg');
+      });
+
+      it('should match previous lookup screenshot', (done) => {
+        validateScreenshot(done, 'lookup');
+      });
+
+      it('should match previous lookup w/ menu screenshot', async (done) => {
+        await validateScreenshotWithMenu(done, 'lookup-w-menu');
+      });
+
+      it('should match previous disabled lookup screenshot', async (done) => {
+        const btn = element(by.css('#btn-disable-lookup'));
+        await btn.click();
+
+        validateScreenshot(done, 'lookup-disabled');
+      });
+    });
+
+    describe('(xs screens)', () => {
+      beforeEach(() => {
+        SkyHostBrowser.setWindowBreakpoint('xs');
+      });
+
+      it('should match previous lookup screenshot', (done) => {
+        validateScreenshot(done, 'lookup-xs');
+      });
+
+      it('should match previous lookup w/ menu screenshot', async (done) => {
+        await validateScreenshotWithMenu(done, 'lookup-w-menu-xs');
+      });
+    });
+  }
+
+  beforeEach(async () => {
+    await SkyHostBrowser.get('visual/lookup');
   });
 
-  describe('(lg screens)', () => {
-    beforeEach(() => {
-      SkyHostBrowser.setWindowBreakpoint('lg');
+  runTests();
+
+  describe('when modern theme', () => {
+    beforeEach(async () => {
+      await selectTheme('modern', 'light');
     });
 
-    it('should match previous lookup screenshot', (done) => {
-      validateScreenshot(done, 'lookup');
-    });
-
-    it('should match previous lookup w/ menu screenshot', (done) => {
-      validateScreenshotWithMenu(done, 'lookup-w-menu');
-    });
-
-    it('should match previous disabled lookup screenshot', (done) => {
-      const btn = element(by.css('#btn-disable-lookup'));
-      btn.click();
-
-      validateScreenshot(done, 'lookup-disabled');
-    });
+    runTests();
   });
 
-  describe('(xs screens)', () => {
-    beforeEach(() => {
-      SkyHostBrowser.setWindowBreakpoint('xs');
+  describe('when modern theme in dark mode', () => {
+    beforeEach(async () => {
+      await selectTheme('modern', 'dark');
     });
 
-    it('should match previous lookup screenshot', (done) => {
-      validateScreenshot(done, 'lookup-xs');
-    });
-
-    it('should match previous lookup w/ menu screenshot', (done) => {
-      validateScreenshotWithMenu(done, 'lookup-w-menu-xs');
-    });
+    runTests();
   });
+
 });
