0c5e8f932e29a351531fd5df7b80f627360c4def
Alex Kingman <alex.kingman@blackbaud.me>
2021-06-22 14:25:00 -0400

    fixed unit test; PR feedback

--- src/app/public/modules/lookup/lookup.component.spec.ts
+++ src/app/public/modules/lookup/lookup.component.spec.ts
@@ -1243,43 +1243,26 @@ describe('Lookup component', function () {
             })
           );
 
-          it('should add items when scrolling ends',
-            async () => {
+          it('should add items into the DOM on scroll',
+            fakeAsync(() => {
               component.enableShowMore = true;
               fixture.detectChanges();
 
               triggerInputFocus(fixture);
               fixture.detectChanges();
-              await fixture.whenStable();
-              // Not using `clickShowMore` due to it being for `fakeAsync`
-              getShowMoreButton().click();
-              fixture.detectChanges();
-              await fixture.whenStable();
+              tick();
+              clickShowMore(fixture);
 
               expect(getRepeaterItemCount()).toBe(10);
 
               let modalContent = document.querySelector('.sky-modal-content');
-              modalContent.scrollTop = modalContent.scrollHeight;
               SkyAppTestUtility.fireDomEvent(modalContent, 'scroll');
-              fixture.detectChanges();
-              await fixture.whenStable();
-              fixture.detectChanges();
 
               expect(getRepeaterItemCount()).toBe(20);
 
-              modalContent = document.querySelector('.sky-modal-content');
-              modalContent.scrollTop = modalContent.scrollHeight;
-              SkyAppTestUtility.fireDomEvent(modalContent, 'scroll');
-              fixture.detectChanges();
-              await fixture.whenStable();
-              fixture.detectChanges();
-
-              expect(getRepeaterItemCount()).toBe(21);
-
-              (<HTMLElement>document.querySelector('.sky-lookup-show-more-modal-close'))?.click();
-            }
+              closeModal(fixture);
+            })
           );
-
         });
 
         it('should trickle down the add button click event when triggered from the show all modal',
